0dd06a65212abf7140977fffc395673c1c542b59
fcintra4 <github4@faustocintra.com.br>
2021-03-08 13:51:20 +0000

    (08/03) Exercícios 04 e 05

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,16 +12,24 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = <div className="box box--small" style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}>small lightblue box</div>
+const mediumBox = <div className="box box--medium" style={{backgroundColor: 'pink', fontStyle: 'italic'}}>medium pink box</div>
+const largeBox = <div className="box box--large" style={{backgroundColor: 'orange', fontStyle: 'italic'}}>large orange box</div>
+
+// Por padrão, toda função de componente recebe todas as props
+// como um único objeto. Caso queiramos lidar com uma prop em
+// particular, precisamos usar desestruturação para isolá-la.
+function Box({style, size = 'small', ...props}) {
+  const className = 'box--' + size
+  return <div className={"box " + className} style={{fontStyle: 'italic', ...style}} {...props} />
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box id="box1" lang="en" size="small" style={{backgroundColor: 'lightblue'}}>Small LightBlue Box</Box>
+      <Box id="box2" lang="en" size="medium" style={{backgroundColor: 'pink'}}>Medium Pink Box</Box>
+      <Box id="box3" lang="en" size="large" style={{backgroundColor: 'orange'}}>Large Orange Box</Box>
     </div>
   )
 }
